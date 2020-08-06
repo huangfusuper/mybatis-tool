f64d784433faf72b6c784b9f4ad13c3dd4f065a1
huangfu <huangfusuper@163.com>
2020-08-06 23:32:44 +0800

    废弃分页缓存处理器一级分页缓存处理器适配器

--- mybatis-page-plugin/src/main/java/com/zhishu/interceptors/CachePageInterceptor.java
+++ mybatis-page-plugin/src/main/java/com/zhishu/interceptors/CachePageInterceptor.java
@@ -1,9 +1,7 @@
 package com.zhishu.interceptors;
 
-import com.zhishu.adaptations.ExecutorsPageProcessorsAdaptation;
 import com.zhishu.common.dto.Page;
 import com.zhishu.config.CachePageConfig;
-import com.zhishu.processors.executors.ExecutorsPageProcessors;
 import com.zhishu.utils.ParameterObjectUtil;
 import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.CachingExecutor;
@@ -11,7 +9,6 @@ import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.*;
-import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
@@ -26,14 +23,34 @@ import org.apache.ibatis.session.RowBounds;
 public class CachePageInterceptor implements Interceptor {
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        //适配处理器
-        ExecutorsPageProcessors executorsPageProcessors = ExecutorsPageProcessorsAdaptation.executorsPageProcessorsAdaptation((Executor) invocation.getTarget());
-        //没有预设处理器就执行原有逻辑
-        if(executorsPageProcessors == null) {
-            return invocation.proceed();
+        //获取参数
+        Object[] args = invocation.getArgs();
+        CachingExecutor cachingExecutor = (CachingExecutor) invocation.getTarget();
+        //获取jdbc执行器
+        MappedStatement mappedStatement = (MappedStatement) args[0];
+        BoundSql boundSql = mappedStatement.getBoundSql(args[1]);
+        //获取缓存key
+        CacheKey cacheKey = cachingExecutor.createCacheKey(mappedStatement, args[1], (RowBounds) args[2], boundSql);
+        Page page = CachePageConfig.PAGE_CACHE.get(cacheKey);
+        Object result = invocation.proceed();
+        //结果集
+        if (page != null) {
+            //获取分页参数
+            Page pageParam = ParameterObjectUtil.getPageParam(args[1]);
+            if (pageParam != null) {
+                //设置缓存数据
+                pageParam.setResults(page.getResults());
+                pageParam.setPageNo(page.getPageNo());
+                pageParam.setPageSize(page.getPageSize());
+                pageParam.setTotalPage(page.getTotalPage());
+                pageParam.setTotalRecord(page.getTotalRecord());
+            }
+        } else {
+            page = ParameterObjectUtil.getPageParam(args[1]);
+            CachePageConfig.PAGE_CACHE.put(cacheKey, page);
         }
         //执行处理器
-        return executorsPageProcessors.invoker(invocation);
+        return result;
     }
 
     @Override
