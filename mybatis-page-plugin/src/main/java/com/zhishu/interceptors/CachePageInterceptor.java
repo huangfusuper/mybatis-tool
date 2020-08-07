f01333c6ba869e44877de7fdc084728648dedbed
huangfusuper <huangfusuper@163.com>
2020-08-07 14:19:10 +0800

    分页缓存在一级缓存和二级缓存失效的情景修改

--- mybatis-page-plugin/src/main/java/com/zhishu/interceptors/CachePageInterceptor.java
+++ mybatis-page-plugin/src/main/java/com/zhishu/interceptors/CachePageInterceptor.java
@@ -3,11 +3,16 @@ package com.zhishu.interceptors;
 import com.zhishu.common.dto.Page;
 import com.zhishu.config.CachePageConfig;
 import com.zhishu.utils.ParameterObjectUtil;
+import org.apache.ibatis.cache.Cache;
 import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.cache.TransactionalCacheManager;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.apache.ibatis.executor.CachingExecutor;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
@@ -30,6 +35,9 @@ public class CachePageInterceptor implements Interceptor {
         BoundSql boundSql = mappedStatement.getBoundSql(args[1]);
         //获取缓存key
         CacheKey cacheKey = executor.createCacheKey(mappedStatement, args[1], (RowBounds) args[2], boundSql);
+        //验证缓存结构，缓存清理
+        cacheEmpty(executor, mappedStatement, cacheKey);
+
         Page page = CachePageConfig.PAGE_CACHE.get(cacheKey);
         Object result = invocation.proceed();
         //结果集
@@ -52,8 +60,87 @@ public class CachePageInterceptor implements Interceptor {
         return result;
     }
 
+    /**
+     * 刷新分页缓存逻辑
+     *
+     * @param executor        执行器
+     * @param mappedStatement 执行器
+     * @param cacheKey        缓存主键
+     */
+    private void cacheEmpty(Executor executor, MappedStatement mappedStatement, CacheKey cacheKey) {
+
+        //如果是二级缓存
+        if (executor instanceof CachingExecutor) {
+            Cache cache = mappedStatement.getCache();
+            if (cache != null) {
+                cachingLogic(executor, cacheKey, cache);
+            } else {
+                Executor delegate = (Executor) SystemMetaObject.forObject(executor).getValue("delegate");
+                perpetualCacheLogic(delegate, cacheKey);
+            }
+        }
+        //当没有开启二级缓存或者当前mapper没有启用二级缓存的时候 就检测一级缓存
+        if (!(executor instanceof CachingExecutor)) {
+            perpetualCacheLogic(executor, cacheKey);
+        }
+
+    }
+
     @Override
     public Object plugin(Object target) {
         return Plugin.wrap(target, this);
+    }
+
+    /**
+     * 二级缓存处理逻辑
+     * <p>
+     * 一个调用时机：
+     * 1.开启二级缓存且mapper配置二级缓存
+     *
+     * @param executor
+     * @param cacheKey
+     * @param cache
+     */
+    private void cachingLogic(Executor executor, CacheKey cacheKey, Cache cache) {
+        CachingExecutor cachingExecutor = (CachingExecutor) executor;
+        TransactionalCacheManager tcm = (TransactionalCacheManager) SystemMetaObject.forObject(cachingExecutor).getValue("tcm");
+        Object object = tcm.getObject(cache, cacheKey);
+        //如果缓存为空但是page不为空  就删除这个page的缓存
+        if (object == null && CachePageConfig.PAGE_CACHE.containsKey(cacheKey)) {
+            CachePageConfig.PAGE_CACHE.remove(cacheKey);
+            Executor delegate = (Executor) SystemMetaObject.forObject(executor).getValue("delegate");
+            perpetualCacheLogic(delegate, cacheKey);
+        }
+        //如果缓存不为空但是page为空  就删除这个缓存
+        if (object != null && !CachePageConfig.PAGE_CACHE.containsKey(cacheKey)) {
+            //删除这个缓存
+            cache.removeObject(cacheKey);
+            tcm.clear(cache);
+        }
+    }
+
+    /**
+     * 一级缓存校验逻辑
+     * <p>
+     * 调用的三个时机
+     * 1.二级缓存不存在
+     * 2.mapper没有标识二级缓存
+     * 3.没有开启二级缓存
+     *
+     * @param executor
+     * @param cacheKey
+     */
+    private void perpetualCacheLogic(Executor executor, CacheKey cacheKey) {
+        PerpetualCache perpetualCache = (PerpetualCache) SystemMetaObject.forObject(executor).getValue("localCache");
+        Object object = perpetualCache.getObject(cacheKey);
+        //如果缓存为空但是page不为空  就删除这个page的缓存
+        if (object == null && CachePageConfig.PAGE_CACHE.containsKey(cacheKey)) {
+            CachePageConfig.PAGE_CACHE.remove(cacheKey);
+        }
+        //如果缓存不为空但是page为空  就删除这个缓存
+        if (object != null && !CachePageConfig.PAGE_CACHE.containsKey(cacheKey)) {
+            //删除这个缓存
+            perpetualCache.removeObject(cacheKey);
+        }
     }
 }
